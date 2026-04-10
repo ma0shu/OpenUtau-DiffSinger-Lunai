f2ed2beb17f014380d4559ae8f6d6306a9f9388e
ma0shu <myy0222@163.com>
2026-04-10 14:38:24 +0800

    refactor: Remove single app process restriction

--- OpenUtau/Program.cs
+++ OpenUtau/Program.cs
@@ -1,7 +1,6 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -22,14 +21,6 @@ namespace OpenUtau.App {
         public static void Main(string[] args) {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
             InitLogging();
-            string processName = Process.GetCurrentProcess().ProcessName;
-            if (processName != "dotnet") {
-                var exists = Process.GetProcessesByName(processName).Count() > 1;
-                if (exists) {
-                    Log.Information($"Process {processName} already open. Exiting.");
-                    return;
-                }
-            }
             Log.Information($"{Environment.OSVersion}");
             Log.Information($"{RuntimeInformation.OSDescription} " +
                 $"{RuntimeInformation.OSArchitecture} " +
